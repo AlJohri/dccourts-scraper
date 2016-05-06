58d1cc09afbb2f0b1e42e96a5c7185aca1c28b96
Al Johri <al.johri@gmail.com>
2016-05-06 10:28:35 -0400

    check if number of expected results is the same as downloaded, add logging

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 from __future__ import print_function
 from __future__ import division
 
-import string, itertools
+import string, itertools, logging
 from dccourts_scraper import DCCourtsScraper, ScraperException
+
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s %(message)s',
+                    handlers=[logging.FileHandler("scraper.log"),
+                              logging.StreamHandler()])
+
+logging.getLogger('requests').setLevel(logging.WARN)
+
+from blessings import Terminal
+t = Terminal()
 
 alphabet = list(string.ascii_lowercase)
 scraper = DCCourtsScraper()
@@ -14,24 +24,37 @@ last_index = search_queries.index(last_query) if last_query else 0
 
 for fname, lname1, lname2 in search_queries[last_query:]:
 
-    print("Scraping", fname, lname1, lname2)
+    search_query = str((fname, lname1, lname2))
+
+    logging.info("Scraping %s" % search_query)
 
     # initialize search query
     try:
         scraper.start_search(fname, lname1+lname2)
     except ScraperException as e:
-        print(e)
+        logging.exception("scraper encountered an error for query %s" % search_query)
         continue
 
-    # iterate through search result pages
+    # get expected number of results
     metadata = scraper.get_search_metadata()
     num_results = metadata['num_results']
-    print("Expecting %d results" % num_results)
+    logging.info("Expecting %d results for query %s" % (num_results, search_query))
 
+    # iterate through search result pages
+    rows = []
     for i, row in enumerate(scraper.get_search_data()):
         print(i, row)
+        rows.append(row)
+
+    try:
+        assert(num_results == len(rows))
+        logging.info("Downloaded %d results for query %s" % (len(rows), search_query))
+    except AssertionError as e:
+        logging.warn(t.red("for query %s number of expected results %d did not "
+                           "match number of returned results %d" % (search_query, num_results, len(rows))))
 
     print('------------------------------')
+
 
     # # initialize case details page
     # scraper.start_details()
